a7697db181798a9fd1966bda101517fca7b8363d
Mario Kirven <mariokirven.mk@gmail.com>
2024-08-28 15:05:59 -0500

    Add Typing, nexline and Backspace

--- word_game/wordle_master.js
+++ word_game/wordle_master.js
@@ -1,80 +1,104 @@
-console.log("Wordle Clone...")
+console.log("Wordle Clone...");
 
 const playArea = document.querySelector(".play-area");
-const matrixDisplay = document.querySelectorAll(".letter-row");
-const letterElements = document.querySelectorAll(".letter-display");
-let letterElementsMatrix;
-const upperCaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const letterElements = document.querySelectorAll(".letter");
+const upperCaseAlphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
+let usedLetters = 0;
+let rowIndex = 0;
 const totalRows = 6;
 const totalColumns = 5;
-let cursorRowPosition = 0;
-let cursorColumnPosition = 0;
+const matrix = generateMatrix();
 
-
-const matrix = [[], [], [], [], [], [],];
-
-console.log(matrix);
-console.log(matrix);
-console.log(matrixDisplay);
-console.log(letterElements);
-
-generateLetterElementMatrix();
-
-document.addEventListener("keydown", (keyPressed) => handleKeyboardInput(keyPressed));
-
+document.addEventListener("keydown", (keyPressed) =>
+  handleKeyboardInput(keyPressed)
+);
 
 function handleKeyboardInput(keyPressed) {
-    console.log(keyPressed);
-    buttonValue = keyPressed["key"].toUpperCase();
-    if (upperCaseAlphabet.includes(buttonValue)) {
-        handleLetterInputs(buttonValue);
-    } else {
-        code = keyPressed["code"];
-        handleControlInputs(code);
-    }
-    updateDisplay();
+  console.log(keyPressed);
+  buttonValue = keyPressed["key"].toUpperCase();
+  if (upperCaseAlphabet.includes(buttonValue)) {
+    handleLetterInputs(buttonValue);
+  } else {
+    code = keyPressed["code"];
+    handleControlInputs(code);
+  }
+  updateDisplay();
 }
 
 function handleLetterInputs(letter) {
-    console.log(`Letter: ${letter}`);
-    if (matrix[cursorRowPosition].length < totalColumns) {
-        matrix[cursorRowPosition].push(letter);
-
-    } else {
-        console.log("No More Space, press enter");
-    }
-    console.log(matrix);
-
+  console.log(`Letter: ${letter}`);
+  if (usedLetters < 5) {
+    matrix[rowIndex][usedLetters].innerText = letter;
+    usedLetters++;
+  }
 }
 
 function handleControlInputs(code) {
-    console.log(`Command: ${code}`);
-    if (code === "Enter") {
-        cursorRowPosition++;
-    }
+  console.log(`Command: ${code}`);
+  if (code === "Enter" && usedLetters === 5) {
+    //Check if word is valid
+    //Look for matches
+    moveCursorToNextRow();
+  } else if (code === "Backspace") {
+    deleteLastLetter();
+  }
 }
 
-function generateLetterElementMatrix() {
-    let rowIndex = 0;
-    let columnIndex = 0;
-    let elementCounter = 0;
-    for (rowIndex; rowIndex < totalRows; rowIndex++) {
-
-        for (columnIndex; columnIndex < totalColumns; columnIndex++) {
-            letterElementsMatrix[rowIndex][columnIndex].push = letterElements[elementCounter];
-            elementCounter++;
-        }
-
-    }
-    console.log(letterElementsMatrix);
+function moveCursorToNextRow() {
+  if (rowIndex < totalRows - 1) {
+    rowIndex++;
+    usedLetters = 0;
+  } else {
+    console.log("No more rows!");
+  }
 }
 
-function updateDisplay() {
-    for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
-        for (let columnIndex = 0; columnIndex < matrix[cursorRowPosition].length; columnIndex++) {
+function updateDisplay() {}
 
-        }
+function generateMatrix() {
+  letterCounter = 0;
+  const my2DArray = [];
+  for (let i = 0; i < totalRows; i++) {
+    my2DArray[i] = [];
+    for (let j = 0; j < totalColumns; j++) {
+      my2DArray[i][j] = letterElements[letterCounter];
+      letterCounter++;
     }
+  }
+  return my2DArray;
+}
 
+function deleteLastLetter() {
+  if (usedLetters > 0) {
+    usedLetters--;
+    matrix[rowIndex][usedLetters].innerText = "";
+  }
 }
