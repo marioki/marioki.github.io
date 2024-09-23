ab00f6c515011c5461c39d32a9032ddcd355c46a
Mario Kirven <mariokirven.mk@gmail.com>
2024-09-22 19:36:32 -0500

    add click cell to toggle dead or alive

--- game_of_life/game_of_life.js
+++ game_of_life/game_of_life.js
@@ -7,18 +7,39 @@ function initializeGame() {
     const gridWidth = 10;
     cells = document.querySelectorAll(".cell");
     const cellMatrix = generateElementMatrix(cells, gridHeigth, gridWidth);
-    
-}
 
-function generateElementMatrix(elementList, heigth, width) {
-    counter = 0;
-    const my2DArray = [];
-    for (let i = 0; i < heigth; i++) {
-        my2DArray[i] = [];
-        for (let j = 0; j < width; j++) {
-            my2DArray[i][j] = elementList[counter];
-            counter++;
+
+
+    function generateElementMatrix(elementList, heigth, width) {
+        counter = 0;
+        const my2DArray = [];
+        for (let i = 0; i < heigth; i++) {
+            my2DArray[i] = [];
+            for (let j = 0; j < width; j++) {
+                my2DArray[i][j] = elementList[counter];
+                addButtonListener(my2DArray[i][j], j, i);
+                counter++;
+            }
+        }
+        return my2DArray;
+    }
+
+    function addButtonListener(cell, x, y) {
+        cell.onclick = function () {
+            console.log(` Button Clicked: ${x},${y} `);
+            toggleCell(x, y);
         }
     }
-    return my2DArray;
+
+    function toggleCell(x, y) {
+        if (cellMatrix[y][x].classList.contains("alive")) {
+            cellMatrix[y][x].classList = ["cell"];
+        } else {
+            cellMatrix[y][x].classList.add("alive");
+        }
+
+
+
+    }
 }
+
